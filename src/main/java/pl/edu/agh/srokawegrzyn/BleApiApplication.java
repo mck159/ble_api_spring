1b02645ea6198aad7e10c34909a729a13c5ea348
Sroka, Maciej <Maciej.Sroka@sabre.com>
2016-06-25 18:34:48 +0200

    Trying to make db on heroku running

--- src/main/java/pl/edu/agh/srokawegrzyn/BleApiApplication.java
+++ src/main/java/pl/edu/agh/srokawegrzyn/BleApiApplication.java
@@ -2,11 +2,46 @@ package pl.edu.agh.srokawegrzyn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class BleApiApplication {
 
-	public static void main(String[] args) {
+    @Bean
+    public DataSource postgresDataSource() {
+        String databaseUrl = System.getenv("DATABASE_URL");
+
+        URI dbUri;
+        try {
+            dbUri = new URI(databaseUrl);
+        }
+        catch (URISyntaxException e) {
+            return null;
+        }
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':'
+                + dbUri.getPort() + dbUri.getPath();
+
+        org.apache.tomcat.jdbc.pool.DataSource dataSource
+                = new org.apache.tomcat.jdbc.pool.DataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setValidationQuery("SELECT 1");
+        return dataSource;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(BleApiApplication.class, args);
 	}
 }
